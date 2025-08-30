3ae13d2b704e1a7f5eac907ade0b10adcc0f9a65
Agent610 <BhavsarParthD96@Gmail.com>
2025-08-30 19:27:20 -0400

    Started the footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,1 +1,24 @@
-/* Use NewsExplorer */
+import React from "react";
+import ".footer.css";
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__content">
+        <nav className="footer__nav">
+          <Link to="/" className="footer__nav-link">
+            Home
+          </Link>
+          <Link to="/about" className="footer__nav-link">
+            About
+          </Link>
+        </nav>
+
+        <p className="footer__copyright">2025 Created by Parth Sonanitwala</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
